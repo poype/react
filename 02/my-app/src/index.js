eba5e10e96dc927e20f8d53dd87ae44ae2986744
poype <253186926@qq.com>
2023-05-23 09:44:13 +0800

    在jsx中引用JavaScript变量

--- 02/my-app/src/index.js
+++ 02/my-app/src/index.js
@@ -4,6 +4,14 @@ import ReactDOM from 'react-dom/client';
 const title = React.createElement('h1', null, 'Hello React脚手架!!!')
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const jsx = <h1>Hello JSX</h1>
+// 在jsx中引用JavaScript变量
+let name = "Jack"
+
+// 建议用小括号将jsx括起来，但只是建议，不是强制
+const jsx = (
+    <h1 className="title">
+        Hello {name}
+    </h1>
+);
 
 root.render(jsx);
